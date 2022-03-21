3e96c0b16ae50f3be47e52d0c19158c17a8c2643
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-22 00:03:12 +0600

    Style Product Cart, style debug and button position

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,11 +3,19 @@ import './Product.css'
 
 // product component for display
 const Product = (props) => {
-    const {name, img, seller, price, ratings} = props.product;
+    const { name, img, seller, price, ratings } = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>{seller}</small></p>
+                <p><small>{ratings} stars</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
