1bede8ed07c6517be72deacbb320310ca2f0087a
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-21 23:11:20 +0600

    data load successfully done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css'
 
 // Shop component for product card display
 const Shop = () => {
+    // products load from the public/products.json
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
     return (
         <div className='shop-container'>
             <div className="products-container">
-                <h3>This is for products</h3>
+                <h3>This is for products: {products.length}</h3>
             </div>
             <div className="cart-container">
                 <h4>This order summary</h4>
