65dd0171dbea4836b31b94196ba567cc91ddc812
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-21 23:16:06 +0600

    Product component created Initially

---
+++ src/components/Product/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// product component for display
+const Product = () => {
+    return (
+        <div>
+            <h2>This is product</h2>
+        </div>
+    );
+};
+
+export default Product;
