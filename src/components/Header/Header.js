82af0b08119d3d06736b20fb4153880da6510477
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-21 22:47:07 +0600

    Header section completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import logo from '../../images/Logo.svg'
 import './Header.css'
+
+// Header Component 
 const Header = () => {
     return (
         <nav className='header'>
             <img src={logo} alt="" />
+            <div>
+                <a href="/shop">Shop</a>
+                <a href="/orders">Orders</a>
+                <a href="/Inventory">Inventory</a>
+                <a href="/about">About</a>
+            </div>
         </nav>
     );
 };
