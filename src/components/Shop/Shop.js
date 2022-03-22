fa8ea9cceb1482a8a39c0e84254fa6ea19c95e16
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-22 23:46:25 +0600

    Load cart from local storage, find product~

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -8,13 +8,22 @@ import './Shop.css'
 const Shop = () => {
     // products load from the public/products.json
     const [products, setProducts] = useState([]);
-    
+
     //Add to cart 
     const [cart, setCart] = useState([]);
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data));
+    }, []);
+
+    // get cart data from local storage
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        for(const id in storedCart){
+            const addedProduct = products.find(product => product.id === id);
+            console.log(addedProduct)
+        }
     }, []);
 
     const handleAddToCart = (product) => {
