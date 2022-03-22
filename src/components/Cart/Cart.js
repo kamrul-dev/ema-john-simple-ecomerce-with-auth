c761e84a5fe0421920b981307a594d909603f37e
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-22 23:15:28 +0600

    Grand Total price calcualtion done with parseFload and  toFixed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Cart.css'
 
+// Cart component for order summary
 const Cart = ({cart}) => {
     let total = 0;
     let shipping = 0;
@@ -8,7 +9,8 @@ const Cart = ({cart}) => {
         total = total + product.price;
         shipping = shipping + product.shipping;
     }
-    const tax = (total * 0.1).toFixed(2);
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
     return (
         <div className='cart'>
             <h4>This order summary</h4>
@@ -16,7 +18,7 @@ const Cart = ({cart}) => {
             <p>Total price: ${total}</p>
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: ${tax}</p>
-            <h4>Grand Total: </h4>
+            <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
         </div>
     );
 };
