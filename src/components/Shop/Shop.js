a40f38ecba3805e8859f0b1e13d4f1b268ad1c8a
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-22 23:26:31 +0600

    cart data stored in localStorage

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -21,6 +22,7 @@ const Shop = () => {
         // should not write this: cart.push(product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
 
     return (
