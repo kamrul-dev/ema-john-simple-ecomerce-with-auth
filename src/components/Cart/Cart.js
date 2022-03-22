7334e1ccfc30de0933b2b5bdce5c0f6000ddc395
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-22 22:56:50 +0600

    Shipping calculation done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,15 +3,17 @@ import './Cart.css'
 
 const Cart = ({cart}) => {
     let total = 0;
+    let shipping = 0;
     for(const product of cart){
         total = total + product.price;
+        shipping = shipping + product.shipping;
     }
     return (
         <div className='cart'>
             <h4>This order summary</h4>
             <p>Selected Items: {cart.length}</p>
             <p>Total price: ${total}</p>
-            <p>Total Shipping: </p>
+            <p>Total Shipping: ${shipping}</p>
             <p>Tax: </p>
             <h4>Grand Total: </h4>
         </div>
