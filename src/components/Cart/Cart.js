31ead1b6941b0409e3048252a77c52f84da3324d
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-22 22:50:31 +0600

    padding added for cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({cart}) => {
+    
     return (
         <div className='cart'>
             <h4>This order summary</h4>
             <p>Selected Items: {cart.length}</p>
+            <p>Total price: </p>
+            <p>Total Shipping: </p>
+            <p>Tax: </p>
+            <h4>Grand Total: </h4>
         </div>
     );
 };
