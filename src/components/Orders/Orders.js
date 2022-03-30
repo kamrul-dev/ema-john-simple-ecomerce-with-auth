491d24a2f04235c4dc8db095ce54c256b98fae45
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-30 22:59:38 +0600

    More style of Review item and apply Remove Item handler

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -8,6 +8,11 @@ const Orders = () => {
     // products laod from hooks folder where useProducts() functiion declared
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
+
+    const handleRemoveProduct = product => {
+        const rest = cart.filter(pd => pd.id !== product.id);
+        setCart(rest);
+    }
     return (
         <div className='shop-container'>
             <div className='review-items-container'>
@@ -15,6 +20,7 @@ const Orders = () => {
                     cart.map(product => <ReviewItem
                         key={product.id}
                         product={product}
+                        handleRemoveProduct={handleRemoveProduct}
                     ></ReviewItem>)
                 }
             </div>
