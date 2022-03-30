76394085ebaef603b92708f2c5b98e94828afe20
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-30 21:22:05 +0600

    Create useCart with products dependency and use it

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 
 const Orders = () => {
     // products laod from hooks folder where useProducts() functiion declared
     const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
     return (
         <div>
             <h2>This is oreders: {products.length}</h2>
+            <p>Cart has: {cart.length}</p>
         </div>
     );
 };
