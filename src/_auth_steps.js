ce7be6849d412dfaf3effb98ebbcd946a06b2d84
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-09 20:37:35 +0600

    installed firebase and Initialize the firebase

---
+++ src/_auth_steps.js
@@ -0,0 +1,4 @@
+/* 
+    1. At First Create a new project in from console.firebase.google.com
+    2. npm install firebase in your firebase project
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLmCBhgt5OjpMP-jTGya8hJL2tHfLkgck",
+  authDomain: "ema-john-simple-ecommerc-87399.firebaseapp.com",
+  projectId: "ema-john-simple-ecommerc-87399",
+  storageBucket: "ema-john-simple-ecommerc-87399.appspot.com",
+  messagingSenderId: "441877262748",
+  appId: "1:441877262748:web:b4f137e276fc17f81e9859"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
