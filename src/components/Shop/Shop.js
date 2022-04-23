454a16147de47d6317fbeded46a721099b66c172
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-24 00:36:05 +0600

    Set Page and Size state and send search query to server

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,11 +9,20 @@ import './Shop.css'
 // Shop component for product card display
 const Shop = () => {
     // // products laod from hooks folder where useProducts() functiion declared
-    const [products, setProducts] = useProducts();
 
     //Add to cart 
     const [cart, setCart] = useState([]);
     const [pageCount, setPageCount] = useState(0);
+    const [page, setPage] = useState(0);
+    const [size, setSize] = useState(10);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/product?page=${page}&size${size}`)
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
+
 
     useEffect(() => {
         fetch('http://localhost:5000/productCount')
@@ -72,8 +81,18 @@ const Shop = () => {
                 <div className='pagination'>
                     {
                         [...Array(pageCount).keys()]
-                            .map(number => <button>{number + 1}</button>)
+                            .map(number => <button
+                                className={page === number ? 'selected' : ''}
+                                onClick={() => setPage(number)}
+                            >{number + 1}</button>)
                     }
+                    {size}
+                    <select onChange={e => setSize(e.target.value)}>
+                        <option value="5">5</option>
+                        <option value="10" selected>10</option>
+                        <option value="15">15</option>
+                        <option value="20">20</option>
+                    </select>
                 </div>
             </div>
             <div className="cart-container">
