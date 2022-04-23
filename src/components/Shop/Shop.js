a24be693ed7ad10f3b3b1a7642276abf84fd5bdc
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-23 22:44:08 +0600

    replace id by _id

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -53,7 +53,7 @@ const Shop = () => {
             <div className="products-container">
                 {
                     products.map(product => <Product
-                        key={product.id}
+                        key={product._id}
                         product={product}
                         handleAddToCart={handleAddToCart}
                     ></Product>)
